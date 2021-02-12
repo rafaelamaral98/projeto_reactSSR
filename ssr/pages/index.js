673db257686ac5546e7ebd4ef2260d9519c5b2b2
rafaelamaral98 <rafaelamaralpessoa658@gmail.com>
2021-02-12 16:02:03 -0300

    Ajustes Finais

--- ssr/pages/index.js
+++ ssr/pages/index.js
@@ -2,14 +2,17 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
+    //Renderização da Página de Contato no lado do Servidor (SSR)
     <div>
+
       <Head>
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossOrigin="anonymous"/>
-      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossOrigin="anonymous"></script>
-      <title>Meu primeiro App NextJS</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossOrigin="anonymous"/>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossOrigin="anonymous"></script>
+        <title>Meu primeiro App NextJS</title>
       </Head>
 
       <body>
+
         <div>
 
           <br/><br/>
@@ -24,8 +27,8 @@ export default function Home() {
 
           <table className="table">
                   
-          <thead> 
-              <tr>
+            <thead> 
+                <tr>
 
                   <td className="text-center w-50">
                       
@@ -41,11 +44,10 @@ export default function Home() {
 
                   </td>
 
-              </tr>
-          </thead>
+                </tr>
+            </thead>
 
           </table>
-
 
           <br/><br/>
 
@@ -62,16 +64,17 @@ export default function Home() {
           </form>
 
           <div className="container-fluid" >
-              <div className="row" >
-                  <div className="col">
-                      <h3>Comentarios de nossos clientes:</h3>
-                  </div>
+            <div className="row" >
+              <div className="col">
+                <h3>Comentarios de nossos clientes:</h3>
               </div>
+            </div>
           </div>
 
           <br/><br/><br/><br/>
 
         </div>
+
       </body>
 
     </div>
